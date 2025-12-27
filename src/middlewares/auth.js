58dd5a9c814c29f0344b1e810e72502e72ce8b68
Camilo Ruas <miloruas@gmail.com>
2025-12-27 07:45:27 -0300

    implantação de middleware de autenticação

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,16 +1,15 @@
 export function authMiddleware(req, res, next) {
   const apiKey = req.headers["x-api-key"];
 
+  console.log("HEADER KEY >>>[" + apiKey + "]<<<");
+  console.log("ENV KEY    >>>[" + process.env.MCP_API_KEY + "]<<<");
+
   if (!apiKey) {
-    return res.status(401).json({
-      error: "Missing API key",
-    });
+    return res.status(401).json({ error: "Missing API key" });
   }
 
   if (apiKey !== process.env.MCP_API_KEY) {
-    return res.status(401).json({
-      error: "Invalid API key",
-    });
+    return res.status(401).json({ error: "Invalid API key" });
   }
 
   next();
